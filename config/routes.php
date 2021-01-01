de643a0cbf6b53d522aa495f4c0e7c9589b233f7
pradeep <pt21388@gmail.com>
2021-01-01 14:31:31 +0530

    Remove extra routes

--- config/routes.php
+++ config/routes.php
@@ -6,11 +6,4 @@
 
 $route->group(['namespace' => 'App\Controllers'], function($route) {
     $route->get('/', 'HomeController@index');
-    $route->get('/users', 'UserController@index');
-    $route->get('/users/:num', 'UserController@findOne');
-    $route->get('/users/:num/edit', 'UserController@showUserForm');
-
-    $route->group(['filters' => ['csrf']], function($route) {
-        $route->post('/users/:num/edit', 'UserController@postUserForm');
-    });
 });
