e86778b5b6c42f76440297172f3663bc90e94d49
pradeep <pt21388@gmail.com>
2021-01-19 23:09:12 +0530

    Tests for data validation library

--- tests/Validator/ValidatorTest.php
+++ tests/Validator/ValidatorTest.php
@@ -256,4 +256,19 @@ final class ValidatorTest extends TestCase
 
         $this->assertFalse($validator->hasErrors());
     }
+
+    public function testValidationRuleBetween()
+    {
+        // Assertion 1
+        $validator = new Validator(['age' => 23]);
+        $validator->setRule('age', 'between:4,8')->run();
+
+        $this->assertTrue($validator->hasErrors());
+
+        // Assertion 2
+        $validator = new Validator(['age' => 23]);
+        $validator->setRule('age', 'between:18,30')->run();
+
+        $this->assertFalse($validator->hasErrors());
+    }
 }
