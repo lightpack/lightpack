100b43a7908fbad62cf946c0fbeb348d3282140d
Pradeep Tiwari <pt21388@gmail.com>
2021-04-29 12:24:07 +0530

    Added cors filter configuration

---
+++ config/cors.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'headers' => [
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Authorization',
+        'Access-Control-Max-Age' => 2 * 60,
+    ],
+];
