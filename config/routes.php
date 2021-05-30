d47954f6d4f14f1c633f0a0c0e71516b6809f716
Pradeep Tiwari <pt21388@gmail.com>
2021-05-30 12:36:00 +0530

    Added routes and controller for CORS support

--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,13 @@
 $route->group(['namespace' => 'App\Controllers'], function($route) {
     $route->get('/', 'HomeController@index');
 });
+
+/**
+ * Register API routes here.
+ */
+
+$route->group(['namespace' => 'App\Controllers\Api'], function ($route) {
+    $route->group(['filters' => ['cors']], function ($route) {
+        $route->options('/api/:any', 'CorsController@index');
+    });
+});
