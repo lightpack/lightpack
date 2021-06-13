4e43d5032b2179370ac8cd30ab3ebd8d8a8a3b4c
pradeep-tiwari <pt21388@gmail.com>
2021-06-13 20:30:27 +0530

    Refactored Lucy CLI console app

--- bootstrap/console.php
+++ bootstrap/console.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/constants.php';
 
 use Lightpack\Console\Console;
 
+/**
+ * Register application specific console commands.
+ */
 $consoleConfig = require_once DIR_CONFIG  . '/console.php';
 
 foreach($consoleConfig as $command => $handler) {
