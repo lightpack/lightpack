1ff399e2e29c90bebb66652c2ee5d5b895cc99aa
Pradeep Tiwari <pt21388@gmail.com>
2021-07-17 14:32:43 +0530

    Removed support for modules

--- config/routes.php
+++ config/routes.php
@@ -4,13 +4,30 @@ use App\Controllers\HomeController;
 use App\Controllers\Api\CorsController;
 
 /**
- * Register app routes here.
+ * @var \Lightpack\Routing\Route
+ */
+
+$route = $container->get('route');
+
+/**
+ * --------------------------------------------------
+ * Register web routes here.
+ * --------------------------------------------------
+ * 
+ * Any route that is not prefixed by 'api' will
+ * be considered as web route.
  */
 
 $route->get('/', HomeController::class);
 
 /**
+ * --------------------------------------------------
  * Register API routes here.
+ * --------------------------------------------------
+ * 
+ * It has been pre-configured with CORS filter so
+ * any cross-origin API request will be served
+ * accordingly.
  */
 
 $route->group(['filters' => ['cors']], function ($route) {
