e67ad18e6bcd01c090c0ccea6b9c9c551ad6f576
Pradeep Tiwari <pt21388@gmail.com>
2021-09-20 15:42:05 +0530

    Move providers into bootstrap directory

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -10,11 +10,31 @@ $container = new Lightpack\Container\Container();
 
 /**
  * ------------------------------------------------------------
- * Bind service providers in container.
+ * List service providers here.
  * ------------------------------------------------------------
  */
 
-$providers = require_once DIR_CONFIG . '/providers.php';
+$providers = [
+    Lightpack\Providers\LogProvider::class,
+    Lightpack\Providers\RouteProvider::class,
+    Lightpack\Providers\EventProvider::class,
+    Lightpack\Providers\CacheProvider::class,
+    Lightpack\Providers\ConfigProvider::class,
+    Lightpack\Providers\RouterProvider::class,
+    Lightpack\Providers\FilterProvider::class,
+    Lightpack\Providers\CookieProvider::class,
+    Lightpack\Providers\SessionProvider::class,
+    Lightpack\Providers\RequestProvider::class,
+    Lightpack\Providers\ResponseProvider::class,
+    Lightpack\Providers\DatabaseProvider::class,
+    Lightpack\Providers\TemplateProvider::class,
+];
+
+/**
+ * ------------------------------------------------------------
+ * Bind service providers in container.
+ * ------------------------------------------------------------
+ */
 
 foreach ($providers as $provider) {
     (new $provider)->register($container);
