25c0e06bf271a0f07c00c3e18d17010e989e7c2c
Pradeep Tiwari <pt21388@gmail.com>
2022-05-26 13:19:02 +0530

    Update bootstrap

--- bootstrap/lucy.php
+++ bootstrap/lucy.php
@@ -40,7 +40,7 @@ require_once DIR_BOOTSTRAP . '/environment.php';
  * ------------------------------------------------------------
  */
 
-require __DIR__ . '/providers.php';
+require_once __DIR__ . '/providers.php';
 
 /**
  * ------------------------------------------------------------
