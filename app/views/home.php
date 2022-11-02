943b928fc8b2d2e95cf9f03ae3d51d59bced3364
Pradeep Tiwari <pt21388@gmail.com>
2022-11-02 19:51:58 +0530

    Update home controller

--- app/views/home.php
+++ app/views/home.php
@@ -3,13 +3,13 @@
 
 <head>
     <title>Lightpack PHP Web Framework</title>
-    <link rel="stylesheet" href="<?= asset_url('css', 'styles.css') ?>" />
-    <link rel="icon" type="image/png" href="<?= asset_url('img', 'logo.ico') ?>" sizes="32x32" />
+    <link rel="stylesheet" href="<?= url()->asset('css/styles.css') ?>" />
+    <link rel="icon" type="image/png" href="<?= url()->asset('img/logo.ico') ?>" sizes="32x32" />
 </head>
 
 <body>
     <div class="container">
-        <img class="logo" src="<?= asset_url('img', 'logo.svg') ?>">
+        <img class="logo" src="<?= url()->asset('img/logo.svg') ?>">
         <h1>Lightpack PHP</h1>
         <p>A modern PHP web framework with extreme performance and small footprint.</p>
     </div>
