6e3be02b2beca98dcbdd794eba44e54767146030
Pradeep Tiwari <pt21388@gmail.com>
2025-08-01 13:30:44 +0530

    refactor: update asset URL helper calls from url()->asset() to asset()->url()

--- app/views/home.php
+++ app/views/home.php
@@ -3,13 +3,13 @@
 
 <head>
     <title>Lightpack PHP Web Framework</title>
-    <link rel="stylesheet" href="<?= url()->asset('css/styles.css') ?>" />
-    <link rel="icon" type="image/png" href="<?= url()->asset('img/logo.ico') ?>" sizes="32x32" />
+    <link rel="stylesheet" href="<?= asset()->url('css/styles.css') ?>" />
+    <link rel="icon" type="image/png" href="<?= asset()->url('img/logo.ico') ?>" sizes="32x32" />
 </head>
 
 <body>
     <div class="container">
-        <img class="logo" src="<?= url()->asset('img/logo.svg') ?>">
+        <img class="logo" src="<?= asset()->url('img/logo.svg') ?>">
         <h1>Lightpack PHP</h1>
         <p>A modern PHP web framework with extreme performance and small footprint.</p>
     </div>
