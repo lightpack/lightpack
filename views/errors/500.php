5df3accc5168fc1d0591bdfd53ca38ea008d9dd5
Pradeep Tiwari <pt21388@gmail.com>
2025-08-09 14:11:30 +0530

    chore: remove legacy error view templates from views/errors directory

--- views/errors/500.php
+++
@@ -1,2 +0,0 @@
-<h2>Technical Error.</h2>
-<p>We are facing some technical issues. We will be back soon.</p>
--- views/errors/503.php
+++
@@ -1,2 +0,0 @@
-<h2>Site down for maintenance.</h2>
-<p>We are upgrading our site experience. We'll be back soon.</p>
